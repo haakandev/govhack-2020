d0e378135e012012e1b22c37a30fae75d66fd3f4
Haakan <haakandev@gmail.com>
2020-08-14 12:12:16 +0000

    Updates

---
+++ precache-manifest.bfd109a73d298afa460b6566455c694a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3541594983b5baeab25689f9655fde",
+    "url": "/govhack-2020/index.html"
+  },
+  {
+    "revision": "354e8215b991d658f2f1",
+    "url": "/govhack-2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c041d26f7945e911879",
+    "url": "/govhack-2020/static/js/2.9f78f03e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/govhack-2020/static/js/2.9f78f03e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "354e8215b991d658f2f1",
+    "url": "/govhack-2020/static/js/main.ae09c0c4.chunk.js"
+  },
+  {
+    "revision": "6f6ae7cceebb55e22c8b",
+    "url": "/govhack-2020/static/js/runtime-main.74ad7797.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/govhack-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
